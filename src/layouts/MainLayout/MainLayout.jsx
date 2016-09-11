04c1717d07d63aa59323ab43abbf5f1cfd7843a3
宮沢ともか <miyazawa_tomoka@yahoo.co.jp>
2016-09-11 23:33:52 +0800

    move nav from layout to app

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -1,7 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { Router, Route, IndexRoute, Link } from 'react-router';
 import styles from './MainLayout.less';
-import Nav from './Nav/Nav'
 
 
 const MainLayout = ({ children }) => {
@@ -11,9 +10,6 @@ const MainLayout = ({ children }) => {
         <div className={styles.headerBg} ></div>
       </header>
       <div className={styles.mainContainer}>
-        <div className={styles.nav}>
-          <Nav />
-        </div>
         {children}
       </div>
 
